b77b35ae23f154593c96bbc9b24903b75049ee3e
rahil24 <rpp2412@gmail.com>
2022-03-16 15:35:44 -0400

    Update artag.py
    corners location prints

--- artag.py
+++ artag.py
@@ -2,23 +2,33 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 import os
-#passing in image and marker size
-def captureid(img,markerSize=6,totalMarkers=250, draw=True):
-	grayScale=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	key=getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
-	arucoDict=aruco.Dictionary_get(key)
-	arucoParam=aruco.DetectorParameters_create()
-	bboxs,ids,rejected=aruco.detectMarkers(grayScale,arucoDict, parameters=arucoParam)
-	print(ids)
-	
+import time
+markerSize=6
+totalMarkers=250
+draw=True
+counter=0
+x=[]
 
+cap=cv2.VideoCapture(0)
+while True:
+    success, img =cap.read()
+    grayScale=cv2.cvtColor(img,cv2.COLORBGR2GRAY)
+    key=getattr(aruco, f'DICT{markerSize}X{markerSize}_{totalMarkers}')
+    arucoDict=aruco.Dictionary_get(key)
+    arucoParam=aruco.DetectorParameters_create()
+    corners,ids,rejected=aruco.detectMarkers(grayScale,arucoDict, parameters=arucoParam)
+    #ids type is numpy.ndarray
+    #NEED TO HAVE ARTAGS ON CAMERA, IF NOT, PROGRAM CRASHES
+    #temp=ids.tolist()
+    print(corners)
+    # if counter==0 and len(ids)==2:
+        # ids.sort()
+        # counter+=1
 
-def main():
-	cap=cv2.VideoCapture(0)
-	while True:
-		success, img =cap.read()
-		captureid(img)
-		cv2.imshow("Image", img)
-		cv2.waitKey(1)
-if __name__ =="__main__":
-   main()
+    #if ids not in x:
+    #    x.insert(counter,counter)
+    #    counter+=1
+    #print(x)
+    #print(ids)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
