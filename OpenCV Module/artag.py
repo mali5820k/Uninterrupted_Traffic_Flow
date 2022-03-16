c9c7b1305885f586c3275b31754fd2e75b7f4e23
mali5820k <mali5820k@gmail.com>
2022-03-16 18:48:27 -0400

    Update artag.py

--- OpenCV Module/artag.py
+++ OpenCV Module/artag.py
@@ -9,38 +9,67 @@ draw=True
 counter=0
 x=[]
 
-def numpyToPy(numpyList):
-    pyList = []
-    for i in numpyList:
-        pyList = numpyList[i][0]
+# WORKS DON'T TOUCH
+def cornersTo2D(corners):
+    corners2D = []
+    for i in range(1, len(corners), 2):
+        corners2D.append([corners[i-1], corners[i]])
+    return corners2D
+
+def getCenter(tagCorners):
+    # TagPos     : idx
+    # TopLeft    : 0
+    # TopRight   : 1
+    # bottomRight: 2
+    # bottomLeft : 3
+    # topRight - topLeft => centerX
+    # topRight - bottomRight => centerY
+    topLeft = tagCorners[0]
+    topRight = tagCorners[1]
+    bottomRight = tagCorners[2]
+    #bottomLeft = tagCorners[3]
     
-    return pyList
+    centerX = (topLeft[0] + topRight[0])/2
+    centerY = (bottomRight[1] - topRight[1])/2 + topRight[1]
+    
+    return [centerX, centerY]
+
+def getTagByID(id, corners):
+    pass
 
 cap=cv2.VideoCapture(0)
 while True:
-    success, img =cap.read()
-    grayScale=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    key=getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
-    arucoDict=aruco.Dictionary_get(key)
-    arucoParam=aruco.DetectorParameters_create()
-    corners,ids,rejected=aruco.detectMarkers(grayScale,arucoDict, parameters=arucoParam)
-    #ids type is numpy.ndarray
-    #NEED TO HAVE ARTAGS ON CAMERA, IF NOT, PROGRAM CRASHES
-    #temp=ids.tolist()
-    temp = np.array(ids)
-    temp = temp.flatten()
-    #temp = numpyToPy(ids)
-    print(corners)
-    print(f"Marker ID is: {temp}")
-    print(f"\nThe length of the corners array is: {len(corners)}\n")
-    # if counter==0 and len(ids)==2:
-        # ids.sort()
-        # counter+=1
-
-    #if ids not in x:
-    #    x.insert(counter,counter)
-    #    counter+=1
-    #print(x)
-    #print(ids)
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    try:
+        success, img =cap.read()
+        grayScale=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        key=getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
+        arucoDict=aruco.Dictionary_get(key)
+        arucoParam=aruco.DetectorParameters_create()
+        corners,ids,rejected=aruco.detectMarkers(grayScale,arucoDict, parameters=arucoParam)
+        centers = []
+        #ids type is numpy.ndarray
+        #NEED TO HAVE ARTAGS ON CAMERA, IF NOT, PROGRAM CRASHES
+        #temp=ids.tolist()
+        
+        temp = np.array(ids)
+        temp = temp.flatten()
+        corners = np.array(corners)
+        corners = corners.flatten()
+        print(corners)
+        corners = cornersTo2D(corners)
+        
+        print(f"Marker ID is: {temp}")
+        print(f"\nThe length of the corners array is: {len(corners)}\n")
+        print(f"Corner Array is: {corners}")
+        centers = getCenter(corners)
+        print(f"Center of Tag is: {centers}")
+        cv2.rectangle(img, (int(corners[0][0]), int(corners[0][1])), (int(corners[2][0]), int(corners[2][1])), (50, 50, 255), 2)
+        cv2.line(img, (int(centers[0]), int(centers[1])), (int(centers[0]), int(centers[1])), (200, 0, 200), 6)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+    except KeyboardInterrupt:
+        print("Quitting Program")
+        break
+    except:
+        print("No tags in camera detection view!\n")
+        continue
