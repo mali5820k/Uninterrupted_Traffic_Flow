a081e5b15c5887bfc1a5140685314f2a4f4e79f8
mali5820k <mali5820k@gmail.com>
2022-04-27 01:40:28 -0400

    Update Event_Listener_From_React.cs

--- App Files/RTFApp/unity/SD App Visualization/Assets/Scripts/Event_Listener_From_React.cs
+++ App Files/RTFApp/unity/SD App Visualization/Assets/Scripts/Event_Listener_From_React.cs
@@ -8,14 +8,16 @@ public class Event_Listener_From_React : MonoBehaviour
     // This class will receive calls from the React-Native side
     public Vector3 user_position;
     public float traffic_light_period_in_seconds = 10f;
+    public int user_camera_resolution_x, user_camera_resolution_y = 1920;
+    public float user_grid_size_x, user_grid_size_y = 28f; // By default this is 28 feet.
 
     public void Awake()
     {
+        UnityMessageManager.Instance.OnMessage += Setup;
         UnityMessageManager.Instance.OnMessage += User_Car_Position;
         UnityMessageManager.Instance.OnMessage += User_Camera_Resolution;
-        UnityMessageManager.Instance.OnMessage += User_GridSize;
-        UnityMessageManager.Instance.OnMessage += User_Green_Arrow_Start;
-        UnityMessageManager.Instance.OnMessage += User_Position;
+        UnityMessageManager.Instance.OnMessage += User_Grid_Size;
+        UnityMessageManager.Instance.OnMessage += User_Green_Arrow_Status;
     }
 
     private void OnDestroy()
@@ -23,9 +25,16 @@ public class Event_Listener_From_React : MonoBehaviour
         
     }
 
+    // Pass in all information to sub-functions
+    // Provided in format "x y, 1920 1080, length height, start"
+    void Setup(string message) {
+        string[] values = message.Split(" ");
+        
+    }
+
     // User Position X, and Y
     // Provided in format "x y"
-    void User_Position(string message)
+    void User_Car_Position(string message)
     {
         string[] values = message.Split(" ");
         float[] converted_values = new float[values.Length];
@@ -39,7 +48,43 @@ public class Event_Listener_From_React : MonoBehaviour
         user_position = new Vector3(converted_values[0], 0f, converted_values[1]);
     }
 
-    void User_(string message) { 
+    // Camera Resolution x by y
+    // Provided in format "1920 1080"
+    void User_Camera_Resolution(string message) {
+        string[] values = message.Split(" ");
+        int[] converted_values = new int[values.Length];
+
+        // Get float from string
+        for (int i = 0; i < values.Length; i++)
+        {
+            int.TryParse(values[i], out int result);
+            converted_values[i] = result;
+        }
+
+        user_camera_resolution_x = converted_values[0];
+        user_camera_resolution_y = converted_values[1];
+    }
+
+    // Sets the length and height of the grid from Real Life Measurements for mapping
+    // Provided in format "length height"
+    void User_Grid_Size(string message) {
+        string[] values = message.Split(" ");
+        float[] converted_values = new float[values.Length];
+
+        // Get float from string
+        for (int i = 0; i < values.Length; i++)
+        {
+            float.TryParse(values[i], out float result);
+            converted_values[i] = result;
+        }
+
+        user_grid_size_x = converted_values[0];
+        user_grid_size_y = converted_values[1];
+    }
+
+    // Set the status of green arrows to start, or reset
+    // Provided in format "start" or "reset"
+    void User_Green_Arrow_Status(string message) { 
         
     }
 
