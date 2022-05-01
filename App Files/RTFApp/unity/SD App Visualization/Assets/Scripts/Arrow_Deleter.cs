24f2242e2874d00b3524aeb3e1c7db35258bed84
mali5820k <mali5820k@gmail.com>
2022-05-01 13:16:59 -0400

    Green-Arrows movement

--- App Files/RTFApp/unity/SD App Visualization/Assets/Scripts/Arrow_Deleter.cs
+++ App Files/RTFApp/unity/SD App Visualization/Assets/Scripts/Arrow_Deleter.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Arrow_Deleter : MonoBehaviour
 {
+    public float block_length = 6f;
     public float speed = 1f;
+    public float green_time = 10f;
 
+    void Start() {
+        speed = block_length / green_time;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.right * speed * Time.deltaTime;
     }
 
     void OnTriggerEnter(Collider collider) {
         Arrow_Spawner other = collider.GetComponent<Arrow_Spawner>();
         if (other) {
-            Destroy(this.gameObject, 4f);
+            if (other.is_receiver)
+                Destroy(this.gameObject, 4f);
         }
     }
 }
