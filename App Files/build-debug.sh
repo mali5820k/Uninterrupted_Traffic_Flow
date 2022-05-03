8ab95bff7f29e592f17b8a5a5af05d9add567698
Alexander Ing <alex@alexing.net>
2022-05-03 10:58:15 -0400

    Updated build scripts

---
+++ App Files/build-debug.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "[BUILD SCRIPT] Running react-native bundle command..."
+cd RTFApp
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+
+echo "[BUILD SCRIPT] Running gradle bundle command..."
+cd android
+./gradlew bundleRelease
+
+echo "[BUILD SCRIPT] Copying apk to current directory"
+cd ../..
+cp ./RTFApp/android/app/build/outputs/apk/debug/app-debug.apk .
