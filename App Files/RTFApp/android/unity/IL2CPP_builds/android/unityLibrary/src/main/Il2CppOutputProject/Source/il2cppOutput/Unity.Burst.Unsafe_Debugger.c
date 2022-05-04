b584c2dd3e4fcec9145a9801a242e19dec8be180
mali5820k <mali5820k@gmail.com>
2022-05-04 10:55:20 -0400

    Mono builds and folder renaming

---
+++ App Files/RTFApp/android/unity/IL2CPP_builds/android/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Unity.Burst.Unsafe_Debugger.c
@@ -0,0 +1,155 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodExecutionContextInfo g_methodExecutionContextInfos[1] = { { 0, 0, 0 } };
+#else
+static const Il2CppMethodExecutionContextInfo g_methodExecutionContextInfos[1] = { { 0, 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const char* g_methodExecutionContextInfoStrings[1] = { NULL };
+#else
+static const char* g_methodExecutionContextInfoStrings[1] = { NULL };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodExecutionContextInfoIndex g_methodExecutionContextInfoIndexes[37] = 
+{
+	{ 0, 0 } /* 0x06000001 T Unity.Burst.Unsafe::Read(System.Void*) */,
+	{ 0, 0 } /* 0x06000002 T Unity.Burst.Unsafe::ReadUnaligned(System.Void*) */,
+	{ 0, 0 } /* 0x06000003 T Unity.Burst.Unsafe::ReadUnaligned(System.Byte&) */,
+	{ 0, 0 } /* 0x06000004 System.Void Unity.Burst.Unsafe::Write(System.Void*,T) */,
+	{ 0, 0 } /* 0x06000005 System.Void Unity.Burst.Unsafe::WriteUnaligned(System.Void*,T) */,
+	{ 0, 0 } /* 0x06000006 System.Void Unity.Burst.Unsafe::WriteUnaligned(System.Byte&,T) */,
+	{ 0, 0 } /* 0x06000007 System.Void Unity.Burst.Unsafe::Copy(System.Void*,T&) */,
+	{ 0, 0 } /* 0x06000008 System.Void Unity.Burst.Unsafe::Copy(T&,System.Void*) */,
+	{ 0, 0 } /* 0x06000009 System.Void* Unity.Burst.Unsafe::AsPointer(T&) */,
+	{ 0, 0 } /* 0x0600000A System.Int32 Unity.Burst.Unsafe::SizeOf() */,
+	{ 0, 0 } /* 0x0600000B System.Void Unity.Burst.Unsafe::CopyBlock(System.Void*,System.Void*,System.UInt32) */,
+	{ 0, 0 } /* 0x0600000C System.Void Unity.Burst.Unsafe::CopyBlock(System.Byte&,System.Byte&,System.UInt32) */,
+	{ 0, 0 } /* 0x0600000D System.Void Unity.Burst.Unsafe::CopyBlockUnaligned(System.Void*,System.Void*,System.UInt32) */,
+	{ 0, 0 } /* 0x0600000E System.Void Unity.Burst.Unsafe::CopyBlockUnaligned(System.Byte&,System.Byte&,System.UInt32) */,
+	{ 0, 0 } /* 0x0600000F System.Void Unity.Burst.Unsafe::InitBlock(System.Void*,System.Byte,System.UInt32) */,
+	{ 0, 0 } /* 0x06000010 System.Void Unity.Burst.Unsafe::InitBlock(System.Byte&,System.Byte,System.UInt32) */,
+	{ 0, 0 } /* 0x06000011 System.Void Unity.Burst.Unsafe::InitBlockUnaligned(System.Void*,System.Byte,System.UInt32) */,
+	{ 0, 0 } /* 0x06000012 System.Void Unity.Burst.Unsafe::InitBlockUnaligned(System.Byte&,System.Byte,System.UInt32) */,
+	{ 0, 0 } /* 0x06000013 T Unity.Burst.Unsafe::As(System.Object) */,
+	{ 0, 0 } /* 0x06000014 T& Unity.Burst.Unsafe::AsRef(System.Void*) */,
+	{ 0, 0 } /* 0x06000015 T& Unity.Burst.Unsafe::AsRef(T&) */,
+	{ 0, 0 } /* 0x06000016 TTo& Unity.Burst.Unsafe::As(TFrom&) */,
+	{ 0, 0 } /* 0x06000017 T& Unity.Burst.Unsafe::Unbox(System.Object) */,
+	{ 0, 0 } /* 0x06000018 T& Unity.Burst.Unsafe::Add(T&,System.Int32) */,
+	{ 0, 0 } /* 0x06000019 System.Void* Unity.Burst.Unsafe::Add(System.Void*,System.Int32) */,
+	{ 0, 0 } /* 0x0600001A T& Unity.Burst.Unsafe::Add(T&,System.IntPtr) */,
+	{ 0, 0 } /* 0x0600001B T& Unity.Burst.Unsafe::AddByteOffset(T&,System.IntPtr) */,
+	{ 0, 0 } /* 0x0600001C T& Unity.Burst.Unsafe::Subtract(T&,System.Int32) */,
+	{ 0, 0 } /* 0x0600001D System.Void* Unity.Burst.Unsafe::Subtract(System.Void*,System.Int32) */,
+	{ 0, 0 } /* 0x0600001E T& Unity.Burst.Unsafe::Subtract(T&,System.IntPtr) */,
+	{ 0, 0 } /* 0x0600001F T& Unity.Burst.Unsafe::SubtractByteOffset(T&,System.IntPtr) */,
+	{ 0, 0 } /* 0x06000020 System.IntPtr Unity.Burst.Unsafe::ByteOffset(T&,T&) */,
+	{ 0, 0 } /* 0x06000021 System.Boolean Unity.Burst.Unsafe::AreSame(T&,T&) */,
+	{ 0, 0 } /* 0x06000022 System.Boolean Unity.Burst.Unsafe::IsAddressGreaterThan(T&,T&) */,
+	{ 0, 0 } /* 0x06000023 System.Boolean Unity.Burst.Unsafe::IsAddressLessThan(T&,T&) */,
+	{ 0, 0 } /* 0x06000024 System.Void System.Runtime.Versioning.NonVersionableAttribute::.ctor() */,
+	{ 0, 0 } /* 0x06000025 System.Void System.Runtime.CompilerServices.IsReadOnlyAttribute::.ctor() */,
+};
+#else
+static const Il2CppMethodExecutionContextInfoIndex g_methodExecutionContextInfoIndexes[1] = { { 0, 0} };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+extern Il2CppSequencePoint g_sequencePointsUnity_Burst_Unsafe[];
+Il2CppSequencePoint g_sequencePointsUnity_Burst_Unsafe[1] = { { 0, 0, 0, 0, 0, 0, 0, kSequencePointKind_Normal, 0, 0, } };
+#else
+extern Il2CppSequencePoint g_sequencePointsUnity_Burst_Unsafe[];
+Il2CppSequencePoint g_sequencePointsUnity_Burst_Unsafe[1] = { { 0, 0, 0, 0, 0, 0, 0, kSequencePointKind_Normal, 0, 0, } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppCatchPoint g_catchPoints[1] = { { 0, 0, 0, 0, } };
+#else
+static const Il2CppCatchPoint g_catchPoints[1] = { { 0, 0, 0, 0, } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppSequencePointSourceFile g_sequencePointSourceFiles[1] = { NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#else
+static const Il2CppSequencePointSourceFile g_sequencePointSourceFiles[1] = { NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppTypeSourceFilePair g_typeSourceFiles[1] = { { 0, 0 } };
+#else
+static const Il2CppTypeSourceFilePair g_typeSourceFiles[1] = { { 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodScope g_methodScopes[1] = { { 0, 0 } };
+#else
+static const Il2CppMethodScope g_methodScopes[1] = { { 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodHeaderInfo g_methodHeaderInfos[37] = 
+{
+	{ 0, 0, 0 } /* T Unity.Burst.Unsafe::Read(System.Void*) */,
+	{ 0, 0, 0 } /* T Unity.Burst.Unsafe::ReadUnaligned(System.Void*) */,
+	{ 0, 0, 0 } /* T Unity.Burst.Unsafe::ReadUnaligned(System.Byte&) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::Write(System.Void*,T) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::WriteUnaligned(System.Void*,T) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::WriteUnaligned(System.Byte&,T) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::Copy(System.Void*,T&) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::Copy(T&,System.Void*) */,
+	{ 0, 0, 0 } /* System.Void* Unity.Burst.Unsafe::AsPointer(T&) */,
+	{ 0, 0, 0 } /* System.Int32 Unity.Burst.Unsafe::SizeOf() */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::CopyBlock(System.Void*,System.Void*,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::CopyBlock(System.Byte&,System.Byte&,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::CopyBlockUnaligned(System.Void*,System.Void*,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::CopyBlockUnaligned(System.Byte&,System.Byte&,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::InitBlock(System.Void*,System.Byte,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::InitBlock(System.Byte&,System.Byte,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::InitBlockUnaligned(System.Void*,System.Byte,System.UInt32) */,
+	{ 0, 0, 0 } /* System.Void Unity.Burst.Unsafe::InitBlockUnaligned(System.Byte&,System.Byte,System.UInt32) */,
+	{ 0, 0, 0 } /* T Unity.Burst.Unsafe::As(System.Object) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::AsRef(System.Void*) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::AsRef(T&) */,
+	{ 0, 0, 0 } /* TTo& Unity.Burst.Unsafe::As(TFrom&) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::Unbox(System.Object) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::Add(T&,System.Int32) */,
+	{ 0, 0, 0 } /* System.Void* Unity.Burst.Unsafe::Add(System.Void*,System.Int32) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::Add(T&,System.IntPtr) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::AddByteOffset(T&,System.IntPtr) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::Subtract(T&,System.Int32) */,
+	{ 0, 0, 0 } /* System.Void* Unity.Burst.Unsafe::Subtract(System.Void*,System.Int32) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::Subtract(T&,System.IntPtr) */,
+	{ 0, 0, 0 } /* T& Unity.Burst.Unsafe::SubtractByteOffset(T&,System.IntPtr) */,
+	{ 0, 0, 0 } /* System.IntPtr Unity.Burst.Unsafe::ByteOffset(T&,T&) */,
+	{ 0, 0, 0 } /* System.Boolean Unity.Burst.Unsafe::AreSame(T&,T&) */,
+	{ 0, 0, 0 } /* System.Boolean Unity.Burst.Unsafe::IsAddressGreaterThan(T&,T&) */,
+	{ 0, 0, 0 } /* System.Boolean Unity.Burst.Unsafe::IsAddressLessThan(T&,T&) */,
+	{ 0, 0, 0 } /* System.Void System.Runtime.Versioning.NonVersionableAttribute::.ctor() */,
+	{ 0, 0, 0 } /* System.Void System.Runtime.CompilerServices.IsReadOnlyAttribute::.ctor() */,
+};
+#else
+static const Il2CppMethodHeaderInfo g_methodHeaderInfos[1] = { { 0, 0, 0 } };
+#endif
+IL2CPP_EXTERN_C const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnity_Burst_Unsafe;
+const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnity_Burst_Unsafe = 
+{
+	(Il2CppMethodExecutionContextInfo*)g_methodExecutionContextInfos,
+	(Il2CppMethodExecutionContextInfoIndex*)g_methodExecutionContextInfoIndexes,
+	(Il2CppMethodScope*)g_methodScopes,
+	(Il2CppMethodHeaderInfo*)g_methodHeaderInfos,
+	(Il2CppSequencePointSourceFile*)g_sequencePointSourceFiles,
+	0,
+	(Il2CppSequencePoint*)g_sequencePointsUnity_Burst_Unsafe,
+	0,
+	(Il2CppCatchPoint*)g_catchPoints,
+	0,
+	(Il2CppTypeSourceFilePair*)g_typeSourceFiles,
+	(const char**)g_methodExecutionContextInfoStrings,
+};
---
+++ App Files/RTFApp/android/unity/IL2CPP_builds/android/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Unity.RenderPipeline.Universal.ShaderLibrary_CodeGen.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnity_RenderPipeline_Universal_ShaderLibrary;
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Unity_RenderPipeline_Universal_ShaderLibrary_CodeGenModule;
+const Il2CppCodeGenModule g_Unity_RenderPipeline_Universal_ShaderLibrary_CodeGenModule = 
+{
+	"Unity.RenderPipeline.Universal.ShaderLibrary.dll",
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	&g_DebuggerMetadataRegistrationUnity_RenderPipeline_Universal_ShaderLibrary,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
