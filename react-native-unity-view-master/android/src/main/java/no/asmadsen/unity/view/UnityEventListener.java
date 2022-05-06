37a047bc19178c093b6e4d6482c6115d3f15b296
Alexander Ing <alex@surfacebook.local>
2022-05-06 10:14:58 -0400

    Remove unused folder

--- react-native-unity-view-master/android/src/main/java/no/asmadsen/unity/view/UnityEventListener.java
+++
@@ -1,5 +0,0 @@
-package no.asmadsen.unity.view;
-
-public interface UnityEventListener {
-    void onMessage(String message);
-}
--- react-native-unity-view-master/ios/UnityNativeModule.h
+++
@@ -1,6 +0,0 @@
-#import <React/RCTEventDispatcher.h>
-#import <React/RCTBridgeModule.h>
-#import "UnityUtils.h"
-
-@interface UnityNativeModule : NSObject <RCTBridgeModule, UnityEventListener>
-@end
